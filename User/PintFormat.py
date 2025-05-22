a7bfcefe2574282b9b073f547c6578c5189f9b90
Benjamin Röjder Delnavaz <benjamin@nobox.se>
2025-05-22 11:31:00 +0200

    ✨ Update PintFormat plugin to ignore vendors

--- User/PintFormat.py
+++ User/PintFormat.py
@@ -5,10 +5,32 @@ import os
 
 
 class PintFormatCommand(sublime_plugin.EventListener):
+    IGNORED_FOLDERS = ["wp", "vendor"]
+
+    def find_closest_config(self, view):
+        filename = view.file_name()
+        path = view.file_name()
+        parent_folder = view.window().folders()[0]
+
+        while path != parent_folder:
+            path = os.path.dirname(path)
+
+            config = f"{path}/pint.json"
+
+            if os.path.exists(config):
+                return config
+
+        return None
+
     def on_post_save_async(self, view):
         filename = view.file_name()
         if filename is None or (not filename.endswith('.php') or filename.endswith('.blade.php')):
             return
+
+        for folder in self.IGNORED_FOLDERS:
+            if folder in filename:
+                print(f"Skipping file {filename} because it's in an ignored folder ('{folder}')")
+                return None
 
         if view.window() is None:
             return
@@ -19,15 +41,15 @@ class PintFormatCommand(sublime_plugin.EventListener):
         if not os.path.exists(pint):
             return
 
-        args = []
+        args: list[str] = []
 
         if os.name == 'nt':
             pint += '.bat'
 
         args.append(pint)
 
-        pint_conf = os.path.normpath(f'{folder}/pint.json')
-        if os.path.exists(pint_conf):
+        pint_conf = self.find_closest_config(view)
+        if pint_conf is not None:
             args.append('--config')
             args.append(pint_conf)
 
@@ -36,6 +58,6 @@ class PintFormatCommand(sublime_plugin.EventListener):
         print("Formatting file", filename)
 
         if os.name == 'nt':
-            subprocess.run(args, creationflags=subprocess.CREATE_NO_WINDOW)
+            _ = subprocess.run(args, creationflags=subprocess.CREATE_NO_WINDOW)
         else:
-            subprocess.run(args)
+            _ = subprocess.run(args)
